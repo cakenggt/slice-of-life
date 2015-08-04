d7a24d66361d1dc02a07547a48764ca67941645a
Alec Lownes <alec.lownes@gmail.com>
2015-08-04 08:37:25 -0400

    Remove tiles entry from maps

--- maps/map1.js
+++ maps/map1.js
@@ -9,6 +9,5 @@ setGameVarForMap({
   playerWidth:0.9,
   scale:1,
   climbableBlocks:0,
-  tiles:[],
   troxelLink:'https://cakenggt.github.io/Troxel/#m=CAgIAFUAAwCAQAD/ALk3yP8R//8A8J4BgwACAAODAAKFAAKFAAKAAJ4BgwAChQAChQAChQACgACeAYMAAoUAAoUAAoUAAoAAngGDAAKFAAKFAAKFAAKAAJ4BngCeAZ4AngGeAJ4BngA='
 });
